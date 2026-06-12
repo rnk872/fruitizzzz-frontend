4e373f02d75a4498e6783d2d68c26d88715cd8d9
Raunak Raj <raunakr1313@gmail.com>
2026-06-12 11:12:11 +0530

    complete professional UI + auth system

--- auth.js
+++ auth.js
@@ -1,29 +1,12 @@
 const API = "https://fruitizzzz-backend.onrender.com";
 
-/* Save token */
-function saveToken(token) {
-  localStorage.setItem("token", token);
-}
-
-/* Get token */
-function getToken() {
-  return localStorage.getItem("token");
-}
-
-/* Logout */
 function logout() {
   localStorage.removeItem("token");
   window.location.href = "login.html";
 }
 
-/* Check login */
-function isLoggedIn() {
-  return !!localStorage.getItem("token");
-}
-
-/* Protect page */
 function protectPage() {
-  if (!isLoggedIn()) {
+  if (!localStorage.getItem("token")) {
     window.location.href = "login.html";
   }
 }
